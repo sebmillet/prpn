2ab6633dca528afa5b8f69683292c34c5ebe5dfc
Sébastien Millet <milletseb@laposte.net>
2010-07-04 15:49:07 +0000

    Continuing to write support for French documentation

--- doc/make-man-page.sh
+++ doc/make-man-page.sh
@@ -4,29 +4,19 @@
 # Generates help documents from binary executable (yes, really!)
 
 SRCDIR="$1"
+LG="$2"
 BASE=pRPN
 
-for LG in en fr; do
-	if [ ${LG} = "en" ]; then
-		MANDIR=man
-	else
-		MANDIR=man/${LG}
-	fi
-	MANPAGE=${MANDIR}/${BASE}.1
-	CMDS=${SRCDIR}/cmds${LG}.txt
-	PREFIX=${SRCDIR}/pRPN1${LG}-prefix
-	POSTFIX=${SRCDIR}/pRPN1${LG}-postfix
-	HTMLDOC=${BASE}${LG}.html
-	TEXTDOC=${BASE}${LG}.txt
-	mkdir -p $MANDIR
-	cat ${PREFIX} > ${MANPAGE}
-	egrep "\([[:digit:]]\):" ${CMDS} | sed 's/^\(\S\+\)\s\+(\(.\)):\s\+/.TP\n.B \1\\ (\2)\n/' >> ${MANPAGE}
-
-#	echo ".SH \"OBJECT TYPES\"" >> ${MANPAGE}
-
-	echo ".TP" >> ${MANPAGE}
-
-#	sed -n '/^Object type/,$p' ${CMDS} | egrep -v "get_class_count" | sed 's/$/\n.br/' | sed 's/^Object type/       Object type/' >> ${MANPAGE}
-
-	cat ${POSTFIX} >> ${MANPAGE}
-done
+if [ ${LG} = "en" ]; then
+	MANPAGE=${BASE}.1
+else
+	MANPAGE=${BASE}.${LG}.1
+fi
+CMDS=${SRCDIR}/cmds${LG}.txt
+PREFIX=${SRCDIR}/pRPN1${LG}-prefix
+POSTFIX=${SRCDIR}/pRPN1${LG}-postfix
+HTMLDOC=${BASE}${LG}.html
+TEXTDOC=${BASE}${LG}.txt
+cat ${PREFIX} > ${MANPAGE}
+egrep "\([[:digit:]]\):" ${CMDS} | sed 's/^\(\S\+\)\s\+(\(.\)):\s\+/.TP\n.B \1\\ (\2)\n/' >> ${MANPAGE}
+cat ${POSTFIX} >> ${MANPAGE}
