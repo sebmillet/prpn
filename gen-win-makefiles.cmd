52e25597569be091c646fd4954116a9597a88576
Sébastien Millet <milletseb@laposte.net>
2010-07-14 13:07:39 +0000

    Started to compile under Windows

---
+++ gen-win-makefiles.cmd
@@ -0,0 +1,29 @@
+@REM
+@REM gen-win-makefiles.cmd
+@REM
+@REM Update the two variables below according to your environment
+@REM Sébastien Millet, 2010
+@REM
+
+@set DIR_INCLUDE_LIBINTL="C:\Progra~1\GnuWin32\include"
+@set DIR_LIB_LIBINTL="C:\Progra~1\GnuWin32\lib"
+
+
+@echo WxWidgets directory (WXWIN variable):     %WXWIN%
+@echo gettext include path (to find libintl.h): %DIR_INCLUDE_LIBINTL%
+@echo gettext lib path (to find libintl.lib):   %DIR_LIB_LIBINTL%
+@pause
+
+@set MYBKL=prpn.bkl
+bakefile -f borland %MYBKL% -I %WXWIN%\build\bakefiles\wxpresets -DMYINCLUDE=%DIR_INCLUDE_LIBINTL% -DMYLIB=%DIR_LIB_LIBINTL%
+bakefile -f msvc %MYBKL% -I %WXWIN%\build\bakefiles\wxpresets -DMYINCLUDE=%DIR_INCLUDE_LIBINTL% -DMYLIB=%DIR_LIB_LIBINTL%
+bakefile -f mingw %MYBKL% -I %WXWIN%\build\bakefiles\wxpresets -DMYINCLUDE=%DIR_INCLUDE_LIBINTL% -DMYLIB=%DIR_LIB_LIBINTL%
+
+cd src
+
+@set MYBKL=dev.bkl
+bakefile -f borland %MYBKL% -I %WXWIN%\build\bakefiles\wxpresets -DMYINCLUDE=%DIR_INCLUDE_LIBINTL% -DMYLIB=%DIR_LIB_LIBINTL%
+bakefile -f msvc %MYBKL% -I %WXWIN%\build\bakefiles\wxpresets -DMYINCLUDE=%DIR_INCLUDE_LIBINTL% -DMYLIB=%DIR_LIB_LIBINTL%
+bakefile -f mingw %MYBKL% -I %WXWIN%\build\bakefiles\wxpresets -DMYINCLUDE=%DIR_INCLUDE_LIBINTL% -DMYLIB=%DIR_LIB_LIBINTL%
+
+cd ..
